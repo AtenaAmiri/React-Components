7c49869f9ba3c0e774e492ee2908ea81fcbfc61e
atena <ftemeh.amiirii@gmail.com>
2023-04-01 21:35:12 +0430

    Magic-Popup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
-import SlideToUnlock from "./projects/slide-to-unlock/SlideToUnlock";
+import SlideToUnlock from "./projects/08-slide-to-unlock/SlideToUnlock";
 import SliderApp from "./projects/09-slider/SliderApp";
 import HiddenSearchBarApp from "./projects/10-hidden-search-bar/HiddenSearchBarApp";
 import EsignatureApp from "./projects/01-signature-app/EsignatureApp";
 import RandomizeColors from "./projects/02-Randomize-colors/RandomizeColors";
+import PopupApp from "./projects/11-Mgic-popup/PopupApp";
 function App() {
   return (
     <div>
-      <RandomizeColors />
+      {/* <RandomizeColors /> */}
       {/* <EsignatureApp /> */}
       {/* <SlideToUnlock /> */}
       {/* <SliderApp /> */}
       {/* <HiddenSearchBarApp /> */}
+      <PopupApp />
     </div>
   );
 }
