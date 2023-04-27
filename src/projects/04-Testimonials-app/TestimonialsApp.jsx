012fa8498fbbffd79127243f1505a02f1cfeff86
atena <ftemeh.amiirii@gmail.com>
2023-04-27 08:39:22 +0430

    1

--- src/projects/04-Testimonials-app/TestimonialsApp.jsx
+++ src/projects/04-Testimonials-app/TestimonialsApp.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import Title from "../components/Title";
+import Button from "../components/Button";
+import { BsFillFileEarmarkPostFill } from "react-icons/bs";
+import { FaUserAlt } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
 
 export default function TestimonialsApp() {
-  return <div>TestimonialsApp</div>;
+  return (
+    <div className="container m-auto">
+      <Title text="Testimonials App" />
+      <Button
+        text={"Posts"}
+        btnClass="btn-info"
+        icon={<BsFillFileEarmarkPostFill />}
+      />
+      <Button btnClass="btn-info" icon={<FaUserAlt />} />
+    </div>
+  );
 }
--- src/projects/components/Button.jsx
+++ src/projects/components/Button.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function Button({ text = "click", btnClass, icon, onClick }) {
+export default function Button({ text = "text", btnClass, icon, onClick }) {
   return (
     <button className={`btn ${btnClass}`} onClick={onClick}>
       {icon}
+      {/* {!text ? "click" : text} */}
       {text}
     </button>
   );
