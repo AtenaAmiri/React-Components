a18cf78e8b9346c3348c254a69d251b0fc4be5e0
atena <ftemeh.amiirii@gmail.com>
2023-04-27 09:01:20 +0430

    1

--- src/projects/04-Testimonials-app/TestimonialsApp.jsx
+++ src/projects/04-Testimonials-app/TestimonialsApp.jsx
@@ -3,18 +3,19 @@ import Title from "../components/Title";
 import Button from "../components/Button";
 import { BsFillFileEarmarkPostFill } from "react-icons/bs";
 import { FaUserAlt } from "react-icons/fa";
-import { FaUserAlt } from "react-icons/fa";
+import { BiCommentDetail } from "react-icons/bi";
 
 export default function TestimonialsApp() {
   return (
     <div className="container m-auto">
       <Title text="Testimonials App" />
       <Button
-        text={"Posts"}
+        text="Posts"
         btnClass="btn-info"
         icon={<BsFillFileEarmarkPostFill />}
       />
-      <Button btnClass="btn-info" icon={<FaUserAlt />} />
+      <Button text="Users" btnClass="btn-info" icon={<FaUserAlt />} />
+      <Button text="Comments" btnClass="btn-info" icon={<BiCommentDetail />} />
     </div>
   );
 }
